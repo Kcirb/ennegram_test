fca3b1959b764b2e7207195a5b7ed4826ac53c41
Kcirb <116576987+Kcirb@users.noreply.github.com>
2024-02-26 10:34:03 +0800

    Update getUserResult.js

--- result/getUserResult.js
+++ result/getUserResult.js
@@ -32,40 +32,11 @@ import getDescriptions from "./getDescription.js";
     sT.appendChild(sTRole);
     
     let sTDescription = document.createElement("p");
-    sTDescription.innerHTML = descriptions[primaryType].description;
     sTDescription.innerHTML = descriptions[secondaryType].description;
     sT.appendChild(sTDescription);
 
 
 })();
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     fetch('../cards/cards.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         let cards = data.filter(card => isInURL(card.id, window.location.href));
-//         let typeScore = [0,0,0,0,0,0,0,0,0];
-//         cards.forEach(card => {
-//             typeScore[card.type-1]++;
-//         });
-//         let arr = typeScore;
-//         const primaryType = typeScore.indexOf(Math.max(...arr))+1
-        
-//         arr.splice(primaryType-1,1,0);
-//         let secondaryType = typeScore.indexOf(Math.max(...arr))+1;
-        
-//         let pT = document.getElementById("primaryType");
-//         let p = document.createElement("p");
-//         p.innerHTML = "您的主要人格類型為：" + primaryType;
-//         pT.appendChild(p);
-
-//         let sT = document.getElementById("secondaryType");
-//         p = document.createElement("p");
-//         p.innerHTML = "您的次要人格類型為：" + secondaryType;
-//         sT.appendChild(p);
-//     })
-//     .catch(error => console.error('Error loading JSON:', error));
-// });
 
 function isInURL(id, url) {
     // 如沒有指定url則使用網頁網址
